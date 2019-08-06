8c9d943025042ae1d751d0e0f627c62f0e834367
Cy <495732128@qq.com>
2019-08-06 14:45:04 +0800

    aftenoon3

--- test1.sh
+++ test1.sh
@@ -2,10 +2,13 @@
 #
 #
 #read -p "Please input a Number: " V1
+while true;
+do
 read -p "Please input you name: " V1
-
 if [ -z "${V1}" ];then
-echo "你没有输入任何信息"
+echo "你没有输入任何信息,请输入： "
 else
-echo "你输入的是 ${V1}"
+echo "你输入的是 ${V1}";
+exit
 fi
+done
