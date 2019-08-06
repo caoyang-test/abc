0586dc0b5075ff14f11cbd5fb83efe9e4bd4b81a
Cy <495732128@qq.com>
2019-08-06 14:39:21 +0800

    aftenoon

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# Author: Cao
+# Date: 20190805
+# Modify: v0.1
+
+ls /root $> /dev/null
+mkdir -p /lesson1
+touch /lesson1/file1
+echo "hello" > /lesson1/file1
+#if
+#echo "执行完成 创建了目录lesson1 编写了文件"
---
+++ test1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#
+#
+read -p "Please input a Number: " V1
+if [ ${V1} -eq 100 ];then
+echo "你刚才输入的是100"
+else
+echo "你刚才输入的不是100"
+fi
