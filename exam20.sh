5d08bd7a323792a61b8230b82a3815f5501148fe
Cy <495732128@qq.com>
2019-08-09 17:24:51 +0800

    fun 1 2 3 4 5 6 7

--- exam20.sh
+++ exam20.sh
@@ -21,7 +21,30 @@ echo "this is fun4"
 return 100
 }
 
+fun5()
+{
+echo "this is fun5 line1"
+return 105
+echo "this is fun5 line2"
+}
+
+fun6()
+{
+echo "this is fun6 line1"
+exit 106
+echo "this is fun6 line2"
+}
+
+
+fun7()
+{
+echo "this is fun6 line1"
+}
+
 fun1
 fun2
 fun3 100 200
 fun4
+fun5
+fun6
+fun7
